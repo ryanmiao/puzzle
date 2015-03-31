b9cb5f8185edf5b778ce6f1d95cfc07245a59c04
lsryan <lsryan815@gmail.com>
2015-03-31 22:45:30 +0800

    Add viewport in html
    Signed-off-by: lsryan <lsryan815@gmail.com>

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var content = '\
     border: 1px solid black; \
   } \
   </style> \
+  <meta name="viewport" content="width=device-width, user-scalable=no" /> \
 </head> \
 <body> \
   <div id="title"> \
