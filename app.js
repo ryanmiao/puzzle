65a8b6968383a7b2045652ed3c711e9ba0602702
lsryan <lsryan815@gmail.com>
2015-04-08 23:54:39 +0800

    modify viewport
    Signed-off-by: lsryan <lsryan815@gmail.com>

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var content = '\
     border: 1px solid black; \
   } \
   </style> \
-  <meta name="viewport" content="width=device-width, user-scalable=no" /> \
+  <meta name="viewport" content="width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" /> \
 </head> \
 <body> \
   <div id="title"> \
